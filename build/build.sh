ba789706e74cb185ca92cc2b0bb4db91968319df
LekKit <50500857+LekKit@users.noreply.github.com>
2025-01-04 00:27:59 +0200

    build: Remove old assets

--- build/build.sh
+++
@@ -1,1 +0,0 @@
-make CC=emcc LDFLAGS="--pre-js pre.js --preload-file fw_jump.bin --preload-file linux_6.2 --preload-file rootfs.img --shell-file shell_minimal.html"
--- build/pre.js
+++
@@ -1,1 +0,0 @@
-Module['arguments'] = ['/fw_jump.bin', '-k', '/linux_6.2', '-i', '/rootfs.img', '-m', '64M'];
