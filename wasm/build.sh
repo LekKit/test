f4ff4b05028336e4a9a1ec8707e1c418acf29f7a
LekKit <50500857+LekKit@users.noreply.github.com>
2025-05-13 14:59:42 +0300

    Update demo to RVVM v0.7-git-r899-7d11e16, introduce xterm.js terminal
    - Full rewrite of index page, do not rely on Emscripten shell.html handling
    - Use .mjs "executable" output for RVVM module
    - Use xterm.js for VT emulation in a way shamelessly stolen from https://vm.estela.moe/
    - Use xterm-pty.mjs for seamlessly plugging xterm.js into Emscripten
    - NOTE: The emscripten-pty.js library has been patched to work without either PROXY_TO_PTHREAD nor ASYNCIFY, but this mainly relies on the fact that RVVM never does blocking read() on stdin. The rationale behind not using ASYNCIFY is simple: performance, and SDL2 conflicts with PROXY_TO_PTHREAD, hence it's made this way

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,1 +1,1 @@
-make CC=emcc LDFLAGS="--pre-js wasm/pre.js --preload-file wasm/fw_jump.bin --preload-file wasm/Image --preload-file wasm/rootfs.img --shell-file wasm/shell_minimal.html"
+make CC=emcc BUILDDIR=wasm LDFLAGS="--preload-file wasm/fw_jump.bin --preload-file wasm/Image --preload-file wasm/rootfs.img --js-library=wasm/emscripten-pty.js"
--- wasm/pre.js
+++
@@ -1,1 +0,0 @@
-Module['arguments'] = ['/wasm/fw_jump.bin', '-k', '/wasm/Image', '-i', '/wasm/rootfs.img', '-m', '64M', '-v'];
